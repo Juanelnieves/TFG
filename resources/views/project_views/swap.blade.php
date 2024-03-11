c090c3a91e7951c291fd88cf01cc3f7a8897fac2
Juan <juangonzalezfernandez004@alumnos.ilernasevilla.com>
2024-03-11 13:41:34 +0100

    modificaciones esteticas

--- resources/views/project_views/swap.blade.php
+++ resources/views/project_views/swap.blade.php
@@ -1,25 +1,7 @@
 @extends('auth.template')
 
 @section('content')
-    {{-- SIDEBAR --}}
-    <div class="container col-1 bg-dark rounded-5 ms-2 d-grid justify-content-center align-items-center">
-        <div></div>
-        <a class="btn rounded-5 btn-lg btn-secondary mt-5" href="{{ url('/pools') }}">
-            <i class="fa-solid fa-person-swimming"></i>
-        </a>
-        <a class="btn rounded-5 btn-lg btn-secondary" href="{{ url('/tokens/all') }}">
-            <i class="fa-brands fa-bitcoin"></i>
-        </a>
-        <a class="btn rounded-5 btn-lg btn-secondary" href="{{ url('/tokens/view') }}">
-            <i class="fa-solid fa-coins"></i>
-        </a>
-        <a class="btn rounded-5 btn-lg btn-secondary" href="{{ url('/transactions/all') }}">
-            <i class="fa-solid fa-receipt"></i>
-        </a>
-        <a class="btn rounded-5 btn-lg btn-secondary mb-5" href="{{ url('/swap') }}">
-            <i class="fa-solid fa-retweet"></i> </a>
-        <div></div>
-    </div>
+   
 
     {{-- CONTENIDO PRINCIPAL --}}
     <div class="container mx-auto mt-5">
